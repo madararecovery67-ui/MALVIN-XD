bbdc101a263dcd18868b2a328e5c1426f601d6e4
madararecovery67-ui <madararecovery67@gmail.com>
2025-08-14 11:27:37 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Ij10EYoZ#30LoaXYoHiuxK9Z8BKImt8MAwm_iBuJ2S6TKnOsFBMs",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2349022323035", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
